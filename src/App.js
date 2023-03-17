4bc74a11d6ae0fcdb36604acba1deb58ebb31174
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-03-17 08:51:16 -0600

    solution code + readme instructions added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [word, setWord] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleChange = (e) => {
+    setWord(e.target.value);
+  };
+
+  const isPalindrome = (word) => {
+    const reversedWord = word.split("").reverse().join("");
+    return word.toLowerCase() === reversedWord.toLowerCase();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isPalindrome(word)) {
+      setResult("This is a palindrome");
+    } else {
+      setResult("This is NOT a palindrome");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a word:
+          <input type="text" value={word} onChange={handleChange} />
+        </label>
+        <button type="submit">Check</button>
+      </form>
+      <p>{result}</p>
+    </div>
+  );
 }
 
 export default App;
